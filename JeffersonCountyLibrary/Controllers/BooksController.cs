443620ef071410b3b61324c09f1f03e1be232bf4
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-10-26 09:38:57 -0600

    - Added mapping for razor pages. - Added authorization so only logged in users can check out books - Added create books link in the nav bar - Added Librarian role to authroization to both the GET & POST Create action in books controller

--- JeffersonCountyLibrary/Controllers/BooksController.cs
+++ JeffersonCountyLibrary/Controllers/BooksController.cs
@@ -24,6 +24,7 @@ namespace JeffersonCountyLibrary.Controllers
         }
 
         // GET: Books/Create
+        [Authorize(Roles ="Libraian")]
         public IActionResult Create()
         {
             ViewData["BranchId"] = new SelectList(_context.Branches, "Id", "Id");
@@ -35,6 +36,7 @@ namespace JeffersonCountyLibrary.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Libraian")]
         public IActionResult Create(Book book)
         {
             book.Branch = _context.Branches.Find(book.BranchId);
@@ -44,6 +46,8 @@ namespace JeffersonCountyLibrary.Controllers
             return RedirectToAction("Details", "Branches", new { id = book.BranchId });
         }
 
+        
+        [Authorize]
         [HttpGet]
         public IActionResult CheckOut(int? id)
         {
